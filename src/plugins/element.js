c58e4440ec81685657924a855f8c3e10c430ebc0
一杆清台鬼见愁 <906449734@qq.com>
2021-09-13 20:07:07 +0800

    fix

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 //导入弹框提示组件
-import {Message} from 'element-ui'
-
-import{Button, Form, FormItem, Input} from 'element-ui'
+import {Button, Form, FormItem, Input, Message} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
